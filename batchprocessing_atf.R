9570e33f57e41f93c11160a68a4ecade9aa241e2
florianfeigl <florian.feigl@gmail.com>
2017-04-08 21:56:17 +0200

    Add files via upload

--- batchprocessing_atf.R
+++ batchprocessing_atf.R
@@ -1,23 +1,53 @@
-# batch processing of atf files from axoscope  
+#!/usr/bin/Rscript
+# setup filedir, title, cols, rows
+cat("Set absolute path to ATF file directory: ")
+f.dir <- readLines(con="stdin", 1)
+f.dir <- as.character(f.dir)
 
-getwd("")
-setwd("")
+cat("Insert plot title: ")
+p.title <- readLines(con="stdin", 1)
+p.title <- as.character(p.title)
+
+cat("Set label of Y axis: ")
+y.lab <- readLines(con="stdin", 1)
+y.lab <- as.character(y.lab)
+
+cat("Set label of X axis: ")
+x.lab <- readLines(con="stdin", 1)
+x.lab <- as.character(x.lab)
+
+cat("Set plots per Y axis for output file: ")
+p.rows <- readLines(con="stdin", 1)
+p.rows <- as.numeric(p.rows)
+
+cat("Set plots per x axis for output file: ")
+p.cols <- readLines(con="stdin", 1)
+p.cols <- as.numeric(p.cols)
 
 # building file index 
+setwd(f.dir)
 files <- c()
-for (i in dir("axonfiles/atf")) {	
+for (i in dir()) {	
 	files = c(files, i) 
 }
 
 # setting up plot functions
 plot_all <- function() {
-par(mfrow=c(3,3))
-for (i in files) {
-	xy <- read.table(i, sep="\t", skip=10, header=TRUE)
-	xy_df <- data.frame(xy[1], xy[2])
-	# pdf file muss noch gemacht werden
-	plot(xy_df, main="Turgor Pressure in Stylus Cells of Lilium longiflorum", # main sollte per Eingabe einstellbar sein!! 
-		 sub=i,  xlab="Time (s)", ylab="Pressure (MPa)", type="l")
+  pdf(file="ALL_PLOTTED_DATA.pdf")
+  par(mfrow=c(p.rows,p.cols))
+  for (i in files) {
+    xy <- read.table(i, sep="\t", skip=10, header=TRUE)
+    xy_df <- data.frame(xy[1], xy[2])
+    plot(xy_df, main=p.title, sub=paste("File: ", i),  xlab=x.lab, 
+         ylab=y.lab, type="l")
+    }
+  dev.off()
 }
-dev.off()
-}
+
+# echo and plotting all data
+print("Plotting all Data...")
+
+plot_all()
+
+# echo
+print("File ALL_PLOTTED_DATA.pdf can be found in the directory of the atf files now!")
