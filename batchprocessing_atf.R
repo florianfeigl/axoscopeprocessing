a60cd5ee7ed220c74d196bc4b50b638a0df9e51f
florianfeigl <florian.feigl@gmail.com>
2017-04-10 20:25:41 +0200

    Update batchprocessing_atf.R

--- batchprocessing_atf.R
+++ batchprocessing_atf.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 # setup filedir, title, cols, rows
-cat("Set absolute path to ATF file directory: ")
+cat("Set path to ATF file directory: ")
 f.dir <- readLines(con="stdin", 1)
 f.dir <- as.character(f.dir)
 
@@ -31,23 +31,19 @@ for (i in dir()) {
 	files = c(files, i) 
 }
 
-# setting up plot functions
-plot_all <- function() {
-  pdf(file="ALL_PLOTTED_DATA.pdf")
-  par(mfrow=c(p.rows,p.cols))
-  for (i in files) {
-    xy <- read.table(i, sep="\t", skip=10, header=TRUE)
-    xy_df <- data.frame(xy[1], xy[2])
-    plot(xy_df, main=p.title, sub=paste("File: ", i),  xlab=x.lab, 
-         ylab=y.lab, type="l")
-    }
-  dev.off()
-}
-
 # echo and plotting all data
 print("Plotting all Data...")
 
-plot_all()
+# setting up plot functions
+pdf(file="ALL_PLOTTED_DATA.pdf")
+par(mfrow=c(p.rows,p.cols))
+for (i in files) {
+  xy <- read.table(i, sep="\t", skip=10, header=TRUE)
+  xy_df <- data.frame(xy[1], xy[2])
+  plot(xy_df, main=p.title, sub=paste("File: ", i),  xlab=x.lab, 
+       ylab=y.lab, type="l")
+  }
+dev.off()
 
 # echo
-print("File ALL_PLOTTED_DATA.pdf can be found in the directory of the atf files now!")
+print("All data processed to the file ALL_PLOTTED_DATA.pdf in the directory of the atf files.")
